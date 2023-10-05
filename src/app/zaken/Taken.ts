610f7959da2ae8a1c25e960296ced6f26efcfccb
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-10-05 10:59:42 +0200

    Fixes baseurl

--- src/app/zaken/Taken.ts
+++ src/app/zaken/Taken.ts
@@ -7,6 +7,7 @@ export class Taken {
   }
 
   async get(zaakId: string) {
+    console.debug(`getting taken for zaak ${zaakId}`);
     const data = await this.client.request(zaakId);
     return data;
   }
