7e2a9c7cd1ff865a766de9cd4896658142c316c6
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-03-05 14:55:42 +0100

    Add class to handle inzendingen

---
+++ src/app/zaken/tests/inzendingen.test.ts
@@ -0,0 +1,24 @@
+import { Bsn } from '@gemeentenijmegen/utils';
+import sampleDownloadLink from './samples/download.json';
+import sampleInzendingen from './samples/list-inzendingen.json';
+import { Inzendingen } from '../Inzendingen';
+import { Person } from '../User';
+
+const inzendingen = new Inzendingen({ baseUrl: 'https://example.com', accessKey: 'test-access-key' });
+jest.spyOn(inzendingen, 'request').mockImplementation(() => Promise.resolve(sampleInzendingen));
+jest.spyOn(inzendingen, 'download').mockImplementation(() => Promise.resolve(sampleDownloadLink));
+
+const person = new Person(new Bsn('900222670'));
+
+describe('Inzendingen from submission storage', () => {
+  test('Getting a list of submissions', async() => {
+    const submissions = await inzendingen.list(person);
+    expect(submissions).toHaveLength(23);
+    expect(submissions.pop()).toHaveProperty('formName');
+  });
+
+  test('Getting a single submission download link', async() => {
+    const submission = await inzendingen.download('APV1.234/APV1.234.pdf');
+    expect(submission).toHaveProperty('downloadUrl');
+  });
+});
